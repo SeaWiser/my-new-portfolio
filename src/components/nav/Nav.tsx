f6c6918e982029f809e1227630c02dc1b0e32326
Yanis <ygraillot@positivethinking.tech>
2023-02-21 11:55:08 +0100

    New components

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './nav.css';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>Nav</div>
+    );
+  }
+}
+
+export default Nav;
