e934222bcc5f8dc404a7da66363ba0a794ac084d
Yanis <ygraillot@positivethinking.tech>
2023-02-23 09:29:43 +0100

    Header component

---
+++ src/components/header/HeaderSocials.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HeaderSocials extends Component {
+  render() {
+    return (
+      <div className='header__socials'>
+        <a href="https://linkedin.com" target="_blank" rel="noreferrer">Linkedin</a>
+        <a href="https://github.com" target="_blank" rel="noreferrer">Github</a>
+      </div>
+    );
+  }
+}
+
+export default HeaderSocials;
