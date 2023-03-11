056e859abbe4f9007586f116f661c22cafc97e47
Yanis <49209290+SeaWiser@users.noreply.github.com>
2023-03-11 19:10:38 +0100

    Favicon + dynamic title with i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
@@ -8,9 +8,13 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Background from "./components/background/Background";
 import './i18n/config';
+import { t } from "i18next";
 
 
 function App() {
+  useEffect(() => {
+    document.title = t('title')
+  }, []);
   return (
     <>
       <Header/>
